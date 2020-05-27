c2b4807ebe551d1bd10a768003138d4dde953211
tit-kacperpol <64531411+tit-kacperpol@users.noreply.github.com>
2020-05-27 11:42:53 +0200

    Add files via upload

---
+++ JavaScript/JS.js
@@ -0,0 +1,73 @@
+function zmien2()
+{
+	document.getElementById('zdj1').src="zdj.1.png";
+}
+function zmien1()
+{
+	document.getElementById('zdj1').src="zdj.2.png";
+}
+/*1zdj*/
+function zmien4()
+{
+	document.getElementById('zdj2').src="zdj.1.png";
+}
+function zmien3()
+{
+	document.getElementById('zdj2').src="maluszek.png";
+} /*2zdj*/
+function zmien6()
+{
+	document.getElementById('zdj3').src="zdj.1.png";
+}
+function zmien5()
+{/*3zdj*/
+	document.getElementById('zdj3').src="ferrari.png";
+}
+function zmien8()
+{
+	document.getElementById('zdj4').src="zdj.1.png";
+}
+function zmien7()
+{
+	document.getElementById('zdj4').src="warszawa.png";
+}
+function zmien10()
+{
+	document.getElementById('zdj5').src="zdj.1.png";
+}
+function zmien9()
+{
+	document.getElementById('zdj5').src="multipla.png";
+}
+function zmien12()
+{
+	document.getElementById('zdj6').src="zdj.1.png";
+}
+function zmien11()
+{
+	document.getElementById('zdj6').src="audi.png";
+}
+function zmien14()
+{
+	document.getElementById('zdj7').src="zdj.1.png";
+}
+function zmien13()
+{
+	document.getElementById('zdj7').src="zabawki.png";
+}
+function zmien16()
+{
+	document.getElementById('zdj8').src="zdj.1.png";
+}
+function zmien15()
+{
+	document.getElementById('zdj8').src="buggati.png";
+}
+function zmien18()
+{
+	document.getElementById('zdj9').src="zdj.1.png";
+}
+function zmien17()
+{
+	document.getElementById('zdj9').src="passat.png";
+}
